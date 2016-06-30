225c3a35944157fe068428f4b21b73105f141480
Maldon Meehan and Stephanie Gurung <stephanie@>
2016-06-30 10:45:57 -0700

    returns 2 quarters and 1 dime if user enters 60 cents

--- lib/make_change.rb
+++ lib/make_change.rb
@@ -1,18 +1,28 @@
 class String
   define_method(:make_change) do
     user_input = self.to_i()
+    change_hash = {"quarters" => 0, "dimes" => 0, "nickels" => 0, "pennies" => 0}
+    temp_hash = Hash.new
     if user_input >= 25
-      number_of_quarters = user_input./(25)
-      number_of_quarters
-    elsif user_input >= 10
-      number_of_dimes = user_input./(10)
-      number_of_dimes
-    elsif user_input >= 5
-      number_of_nickels = user_input./(5)
-      number_of_nickels
-    else
-      number_of_pennies = user_input
-      number_of_pennies
+      number_of_quarters = user_input./(25).floor()
+      temp_hash.store("quarters", number_of_quarters)
+      user_input = user_input.%(25)
     end
+    if user_input >= 10
+      number_of_dimes = user_input./(10)
+      temp_hash.store("dimes", number_of_dimes)
+      user_input = user_input.%(10)
+    end
+    if user_input >= 5
+      number_of_nickels = user_input./(5)
+      temp_hash.store("nickels", number_of_nickels)
+      user_input = user_input.%(5)
+    end
+    if user_input < 5
+      number_of_pennies = user_input
+      temp_hash.store("pennies", number_of_pennies)
+    end
+puts change_hash.merge(temp_hash)
+    change_hash.merge(temp_hash)
   end
 end
--- spec/make_change_spec.rb
+++ spec/make_change_spec.rb
@@ -3,19 +3,22 @@ require('make_change')
 
 describe('String#make_change') do
   it('returns 2 quarters if user enters 50 cents') do
-    expect("50".make_change()).to(eq(2))
+    expect("50".make_change()).to(eq({"quarters" => 2, "dimes" => 0, "nickels" => 0, "pennies" => 0}))
   end
 
   it('returns 2 dimes if user enters 20 cents') do
-    expect("20".make_change()).to(eq(2))
+    expect("20".make_change()).to(eq({"quarters"=>0, "dimes"=>2, "nickels"=>0, "pennies"=>0}))
   end
 
   it('returns 1 nickel if user enters 5 cents') do
-    expect("5".make_change()).to(eq(1))
+    expect("5".make_change()).to(eq({"quarters"=>0, "dimes"=>0, "nickels"=>1, "pennies"=>0}))
   end
 
   it('returns 4 pennies if user enters 4 cents') do
-    expect("4".make_change()).to(eq(4))
+    expect("4".make_change()).to(eq({"quarters"=>0, "dimes"=>0, "nickels"=>0, "pennies"=>4}))
   end
 
+  it('returns 2 quarters and 1 dime if user enters 60 cents') do
+    expect("60".make_change()).to(eq({"quarters"=>2, "dimes"=>1, "nickels"=>0, "pennies"=>0}))
+  end
 end
