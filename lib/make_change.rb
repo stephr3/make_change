efbbb9454cfad62fe6b65aa2e6ce347124cbe3a7
Maldon Meehan and Stephanie Gurung <stephanie@>
2016-06-30 09:57:10 -0700

    returns 2 dimes if user enters 20 cents

--- lib/make_change.rb
+++ lib/make_change.rb
@@ -1,7 +1,12 @@
 class String
   define_method(:make_change) do
     user_input = self.to_i()
-    number_of_quarters = user_input./(25)
-    number_of_quarters
+    if user_input >= 25
+      number_of_quarters = user_input./(25)
+      number_of_quarters
+    elsif user_input >= 10
+      number_of_dimes = user_input./(10)
+      number_of_dimes
+    end
   end
 end
--- spec/make_change_spec.rb
+++ spec/make_change_spec.rb
@@ -5,4 +5,8 @@ describe('String#make_change') do
   it('returns 2 quarters if user enters 50 cents') do
     expect("50".make_change()).to(eq(2))
   end
+
+  it('returns 2 dimes if user enters 20 cents') do
+    expect("20".make_change()).to(eq(2))
+  end
 end
